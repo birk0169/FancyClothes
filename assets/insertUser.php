3fbcef300d806356d46d3481f7e2c5803761d287
Birk Høgh-Mogensen <birk0169@edu.campusvejle.dk>
2019-06-25 11:24:03 +0200

    added registration functionality

---
+++ assets/insertUser.php
@@ -0,0 +1,49 @@
+<?php
+    ob_start();
+
+    //If logged in return user to front page
+    if(isset($_SESSION['username'])){
+        header("location: index.php");
+    }
+
+    //Save data from post in variables
+    $fromUsername = $_POST['formNewUserName'];
+    $formPassword1 = $_POST['formPassword1'];
+    $formPassword2 = $_POST['formPassword2'];
+
+    //Connect to database
+    require "connect.php";
+
+    //check database for any existing users who has the same username
+    $statement = $dbh->prepare("SELECT * FROM users WHERE userName = ?");
+    $statement->bindParam(1, $fromUsername);
+    $statement->execute();
+
+    if($row = $statement->fetch()){
+        //Username is already in use
+        echo "<p class='errorMsg'>ERROR - A user is already using that username!</p>";
+        //For later updates make it so that it returns to the register page with information about what went wrong using GET
+    } else if($formPassword1 != $formPassword2){
+        //Password not confirmed
+        echo "<p class='errorMsg'>ERROR - Password not confirmed!</p>";
+    } else{
+        //Passed - Create new user
+        echo "<p class='errorMsg'>ERROR - Password not confirmed!</p>";
+
+        //Prepare statement
+        $statement = $dbh->prepare("INSERT INTO users(userName, userPassword) VALUES(?, ?)");
+
+        //For later updates add password hash function
+
+        //Bind parameters
+        $statement->bindParam(1, $fromUsername);
+        $statement->bindParam(2, $formPassword1);
+
+        $statement->execute();
+
+        header('location:../index.php');
+
+
+    }
+
+?>
---
+++ register.php
@@ -0,0 +1,29 @@
+<?php
+    require "header.php";
+?>
+
+<div class="createArticle container">
+
+        <h3 class="center errorMsg">Opret Bruger:</h3>
+        <form action="assets/insertUser.php" method="post">
+            <div>
+                <label for="formNewUserName">Bruger Navn</label>
+                <input type="text" id="formNewUserName" name="formNewUserName" placeholder="Intast brugernavn..." required>
+            </div>
+            <div>
+                <label for="formPassword1">Bruger Kode</label>
+                <input type="password" id="formPassword1" name="formPassword1" placeholder="Intast kode..." required>
+            </div>
+            <div>
+                <label for="formPassword2">Bekræft Kode</label>
+                <input type="password" id="formPassword2" name="formPassword2" placeholder="Bekræft kode..." required>
+            </div>
+            <div>
+                <input type="submit" value="Opret" name="value">
+            </div>
+        </form>
+
+    </div>
+
+<?php
+    require "footer.php";
